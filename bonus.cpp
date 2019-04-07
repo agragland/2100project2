d101a12b35838771656ee1087f58182079392f35
agragland <hondakid145@gmail.com>
2019-04-07 17:47:24 -0500

    bonus complete

---
+++ bonus.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int n = 0;
+  int sum = 0;
+  vector<int> digits;
+  cout << "Enter a number less than or equal to 100,000: " << endl;
+  cin >> n;
+  bool finished = false;
+  if(n >= 0)
+  {
+    while(!finished)
+    {
+      string number = to_string(n);
+      for(int i = 0; i < number.size(); i++)
+      {
+        //cout << number.substr(i,i+1) << endl;
+        digits.push_back(stoi(number.substr(i,1)));
+      }
+      //digits.push_back(stoi(number.substr(0,1)));
+      //digits.push_back(stoi(number.substr(1)));
+      for(int k = 0; k < digits.size(); k++)
+      {
+        sum+=digits.at(k);
+      }
+      if(n % sum == 0)
+      {
+        finished = true;
+        cout << "The smallest magic number is " << n << endl;
+      }
+      else
+      {
+        //cout << digits.size() << endl;
+        sum = 0;
+        n++;
+        digits.clear();
+      }
+    }
+  }
+  else
+  {
+    cout << "That number is negative, try a positive number" << endl;
+  }
+
+
+}
